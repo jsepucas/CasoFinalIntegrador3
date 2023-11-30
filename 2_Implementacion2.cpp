1ac935107477d045d26afa009a9a12293f287ca3
jsepucas <jsepucas@myuax.com>
2023-11-30 12:33:50 +0100

    8

--- 2_Implementacion2.cpp
+++ 2_Implementacion2.cpp
@@ -2,3 +2,10 @@
 #include <string>
 #include <cstdio>
 #include <cstring>
+
+using namespace std;
+
+struct ColorConsole {
+    static constexpr auto fg_blue = "\033[34m";
+    static constexpr auto bg_white = "\033[47m";
+};
