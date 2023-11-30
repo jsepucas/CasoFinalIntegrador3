c1fbf1972b6fe23ca0b76ddd20b79012bb872aca
jsepucas <jsepucas@myuax.com>
2023-11-30 12:33:03 +0100

    7

---
+++ 1_Archivo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace
+int resta(int a, int b) {
+    return a - b;
+}
+
+int main() {
+    std::cout<< resta (30, 1) << std::endl;
+
+    std::cout<<"Archivo a sacar en la terminal" << std::endl;
+    return 0;
+
+}
--- 1_Implementacion1.cpp
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-
-int resta(int a, int b) {
-    return a - b;
-}
