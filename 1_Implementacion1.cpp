70d9cd84930e3338792150182d7e502056dacd62
jsepucas <jsepucas@myuax.com>
2023-11-30 12:26:43 +0100

    6

--- 1_Implementacion1.cpp
+++ 1_Implementacion1.cpp
@@ -1,35 +1,5 @@
 #include <iostream>
-#include <fstream>
-#include <string>
 
-using namespace std;
-
-void load_script(const char* filename, bool show_script = false) {
-    ifstream file(filename);
-
-    if(!file.is_open()) {
-        cout << "Error al abrir el archivo" << endl;
-        return;
-    }
-
-    string line;
-    while(getline(file, line)) {
-        if(show_script) {
-            cout << line << endl;
-        }
-    }
-
-    if (!file.eof()) {
-        cout << "Ha ocurrido un error al leer el archivo." << endl;
-    }
-    file.close();
-}
-
-void load_script() {
-    string filename;
-    cout << "Introduce el nombre del archivo: ";
-    cin >> filename;
-
-    load_script(filename.c_str(), true);
-
+int resta(int a, int b) {
+    return a - b;
 }
